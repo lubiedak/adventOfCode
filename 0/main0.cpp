0e1183195c8ae1a2fbabd8c2d9b925f81b7f34ec
Lukasz Biedak <l.biedak@wp.pl>
2016-03-29 16:01:53 +0000

    Task 0 completed

--- 0/main0.cpp
+++ 0/main0.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(){
-  std::cout<<"Hello";
+  //Read file content
+  std::ifstream file("input");
+  std::string input;
+  std::getline(file, input);
+
+  int level = 0;
+  for(std::string::size_type i = 0; i < input.size(); ++i){
+    if(input[i] == '(')
+      ++level;
+    if(input[i] == ')')
+      --level;
+    if(level==-1){
+      std::cout<<i+1<<std::endl;
+      break;
+    }
+  }
   return 0;
 }
