d7471dfc1dc6427f1f4e2212b8a113f06536649d
Lukasz Biedak <l.biedak@wp.pl>
2016-03-29 16:38:59 +0000

    Task 1 - Box class

--- 1/main0.cpp
+++ 1/main0.cpp
@@ -2,16 +2,50 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+
+class Box{
+public:
+  Box(){}
+  Box(std::string str){
+    std::stringstream ss;
+    std::string dim;
+    ss << str;
+
+    std::getline(ss, dim, 'x');
+    l = std::stoi(dim);
+
+    std::getline(ss, dim, 'x');
+    w = std::stoi(dim);
+
+    std::getline(ss, dim);
+    h = std::stoi(dim);
+  }
+
+  void print(){
+    std::cout<<l<<"\t"<<w<<"\t"<<h<<std::endl;
+  }
+
+private:
+  int l;
+  int w;
+  int h;
+  int min;
+};
+
 
 int main(){
   //Read file content
   std::ifstream file("input");
-  std::vector<std::string> input;
+  std::vector<Box> boxes;
   std::string line;
 
   while(std::getline(file, line)){
-    input.push_back(line);
+    boxes.push_back(Box(line));
   }
+
+  for(auto b : boxes)
+    b.print();
 
   return 0;
 }
