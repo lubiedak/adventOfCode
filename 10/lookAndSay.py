a49bdbc2b99f7d5c903ab289a123911502e23426
Lukasz Biedak <lukasz.biedak@powel.com>
2016-08-04 17:06:06 +0200

    Finished 10

---
+++ 10/lookAndSay.py
@@ -0,0 +1,18 @@
+def lookAndSay(word):
+	n = 1
+	oldC=word[0]
+	newWord=""
+	for c in word[1:]:
+		if(oldC!=c):
+			newWord=newWord+str(n)+oldC
+			n=0
+		oldC=c
+		n=n+1
+	newWord=newWord+str(n)+oldC
+	return newWord
+
+word = "1113222113"
+for i in range(50):
+	word = lookAndSay(word)
+
+print(len(word))
