0a78a9188198d4790eb6abe8ebdcedbd86f5d808
Lukasz Biedak <lukasz.biedak@powel.com>
2016-08-09 10:31:53 +0200

    Finished 13

--- 13/OptimalTablePlaces.py
+++ 13/OptimalTablePlaces.py
@@ -1,19 +1,21 @@
 #Alice would gain 2 happiness units by sitting next to Bob.
 
+from itertools import permutations
+
 def readLines():
 	data = ""	
 	with open('input.txt') as f:
 		data = f.readlines()
 	return data
 
-allNames = []
+allNames = set()
 	
 def parseData(data):
 	sittingPreferences = {}
 	for line in data:
 		words = line.split(' ')
 		name1 = words[0]
-		allNames.append(name1)
+		allNames.add(name1)
 		name2 = words[-1][:-2]
 		pref = int(words[3]) * (-1 if (words[2] == "lose") else 1)
 		
@@ -22,3 +24,19 @@ def parseData(data):
 	
 data = readLines()
 preferences = parseData(data)
+
+allPerms = list(permutations(allNames))
+
+best = 0
+sum = 0
+print(preferences["MalloryGeorge"])
+for perm in allPerms:
+	sum=0
+	for i in range(len(perm)):
+		left = i-1 if(i>0) else len(perm)-1
+		right = i+1 if(i<len(perm)-1) else 0
+		sum+=preferences[perm[i]+perm[left]] + preferences[perm[i]+perm[right]]
+	if(sum > best):
+		best = sum
+		print best
+		print perm
